b673de7a91ba255097642cf30bcffeeba87946d1
lakhabhai ghoghol <ghoghollaxman@gmail.com>
2024-10-19 09:47:04 +0530

    feat: handle null values

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,7 +3,7 @@ package org.example;
 public class StringCalculator {
 
     public Integer add(String string) {
-        if(string.isEmpty()){
+        if(string == null || string.isEmpty()){
             return 0;
         }
 

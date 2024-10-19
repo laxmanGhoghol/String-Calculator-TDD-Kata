4afce820ab23a39c3f86d4659e41541339b7773d
lakhabhai ghoghol <ghoghollaxman@gmail.com>
2024-10-19 10:37:18 +0530

    feat: added support for different delimiter

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -8,8 +8,19 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        String[] numbers = input.replace('\n', ',')
-                .split(",");
+        String delimiter = ",";
+        String formattedInput = input;
+        if(input.startsWith("//")){
+            StringBuilder stringBuilder = new StringBuilder(input);
+            int indexOfNewLine = stringBuilder.indexOf("\n");
+            delimiter = ";";
+            stringBuilder.delete(0, indexOfNewLine+1);
+            formattedInput = stringBuilder.toString();
+        }
+
+        String[] numbers = formattedInput.replace('\n', ',')
+                .split(delimiter);
+
         return Arrays.stream(numbers).map(Integer::parseInt)
                 .reduce(0, Integer::sum);
     }

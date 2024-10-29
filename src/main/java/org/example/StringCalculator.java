8402c62a02c759cb257d53986cec09f26895ae4c
lakha <ghoghollaxman@gmail.com>
2024-10-29 15:32:38 +0530

    feat: handle when Asterik is given and handle start and end value

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    public Integer add(String input) {
+    public Integer add(String input, Integer startValue, Integer endValue) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
@@ -27,10 +27,19 @@ public class StringCalculator {
             throw new NegativeNumberException("negative numbers not allowed " + negativeNumbers);
         }
 
+        if(delimiter.equals(Pattern.quote("*"))){
+            return extractIntegers(numbers).stream().reduce(1, (a, b) -> a * b);
+        }
+
+        if(startValue != null && endValue != null){
+            return extractIntegers(numbers).stream().filter((num) -> (num < endValue && num > startValue)).reduce(0, Integer::sum);
+        }
+
         return extractIntegers(numbers).stream().reduce(0, Integer::sum);
     }
 
     private List<Integer> extractIntegers(String[] numbers){
         return Arrays.stream(numbers).map(String::trim).filter(str -> !str.isEmpty()).map(Integer::parseInt).toList();
     }
+
 }
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -9,91 +9,111 @@ class StringCalculatorTest {
 
     @Test
     void shouldReturnZeroWhenGivenEmptyString(){
-        Integer result = stringCalculator.add("");
+        Integer result = stringCalculator.add("", null, null);
 
         assertEquals(0, result);
     }
 
     @Test
     void shouldGiveSumWhenGivenSingleNumber(){
-        Integer result = stringCalculator.add("1");
+        Integer result = stringCalculator.add("1", null, null);
 
         assertEquals(1, result);
     }
 
     @Test
     void shouldGiveSumAsTwoWhenGivenTwoAsSingleNumber(){
-        Integer result = stringCalculator.add("2");
+        Integer result = stringCalculator.add("2", null, null);
 
         assertEquals(2, result);
     }
 
     @Test
     void shouldGiveZeroWhenGivenNullInput(){
-        Integer result = stringCalculator.add(null);
+        Integer result = stringCalculator.add(null, null, null);
 
         assertEquals(0, result);
     }
 
     @Test
     void shouldGiveSumOfGivenTwoNumbers(){
-        Integer result = stringCalculator.add("1,2");
+        Integer result = stringCalculator.add("1,2", null, null);
 
         assertEquals(3, result);
     }
 
     @Test
     void shouldGiveSumOfMultipleNumbersWithNewLinesBetweenAsSeparator(){
-        Integer result = stringCalculator.add("1\n2,3");
+        Integer result = stringCalculator.add("1\n2,3", null, null);
 
         assertEquals(6, result);
     }
 
     @Test
     void shouldSupportGivenDelimitersInInput(){
-        Integer result = stringCalculator.add("//;\n1;2");
+        Integer result = stringCalculator.add("//;\n1;2", null, null);
 
         assertEquals(3, result);
     }
 
     @Test
     void shouldSupportDifferentDelimiterPeriodGivenInInput(){
-        Integer result = stringCalculator.add("//.\n2.2");
+        Integer result = stringCalculator.add("//.\n2.2", null, null);
 
         assertEquals(4, result);
     }
 
     @Test
     void shouldGiveErrorWhenGivenNegativeNumbers(){
-        assertThrows(NegativeNumberException.class, ()-> stringCalculator.add("//,\n-2,4"));
+        assertThrows(NegativeNumberException.class, ()-> stringCalculator.add("//,\n-2,4", null, null));
     }
 
     @Test
     void shouldGiveErrorMessageWithGivenNegativeNumbers(){
-        NegativeNumberException negativeNumberException = assertThrows(NegativeNumberException.class, () -> stringCalculator.add("//,\n-2,4"));
+        NegativeNumberException negativeNumberException = assertThrows(NegativeNumberException.class, () -> stringCalculator.add("//,\n-2,4", null, null));
 
         assertEquals("negative numbers not allowed [-2]", negativeNumberException.getMessage());
     }
 
     @Test
     void shouldGiveErrorMessageWithGivenMultipleNegativeNumbers(){
-        NegativeNumberException negativeNumberException = assertThrows(NegativeNumberException.class, () -> stringCalculator.add("//,\n-2,-4,1,-3,5"));
+        NegativeNumberException negativeNumberException = assertThrows(NegativeNumberException.class, () -> stringCalculator.add("//,\n-2,-4,1,-3,5", null, null));
 
         assertEquals("negative numbers not allowed [-2, -4, -3]", negativeNumberException.getMessage());
     }
 
     @Test
     void shouldIgnoreWhiteSpacesBetween(){
-        Integer result = stringCalculator.add("  2  ,  3 \n 4");
+        Integer result = stringCalculator.add("  2  ,  3 \n 4", null, null);
 
         assertEquals(9, result);
     }
 
     @Test
     void shouldIgnoreWhiteSpacesAfterDelimiter(){
-        Integer result = stringCalculator.add("3, 4, , ,  ");
+        Integer result = stringCalculator.add("3, 4, , ,  ", null, null);
 
         assertEquals(7, result);
+    }
+
+    @Test
+    void shouldGiveMultipliedNumberWhenGivenDelimitterIsAsterik(){
+        Integer result = stringCalculator.add("//*\n2*4", null, null);
+
+        assertEquals(8, result);
+
+    }
+
+    //todo: expected os tjat user should be able to pass start and end value (10 - 20)
+    // "1, 2, 3, 4, 5, 11, 12, 13, 17, 18, 29, 30, 31"
+    @Test
+    void shouldAllowOnlyValuesBetweenTheProvidedRange(){
+        int startValue = 10;
+        int endValue = 20;
+
+        Integer result = stringCalculator.add("1, 2, 3, 4, 5, 11, 12, 13, 17, 18, 29, 30, 31", startValue, endValue);
+
+        assertEquals(71, result);
     }
 
 }
